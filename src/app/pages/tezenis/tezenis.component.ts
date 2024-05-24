87ff628a916b0c3c3fdd00dd278a09462e42facc
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-24 13:49:23 +0200

    chore: Update Tezenis component with images and styling

--- src/app/pages/tezenis/tezenis.component.ts
+++ src/app/pages/tezenis/tezenis.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BikiniService } from '../../bikini.service';
+import { iBikini } from '../../Models/i-bikini';
 
 @Component({
   selector: 'app-tezenis',
   templateUrl: './tezenis.component.html',
-  styleUrl: './tezenis.component.scss'
+  styleUrl: './tezenis.component.scss',
 })
 export class TezenisComponent {
+  bikiniArr: iBikini[] = [];
+  constructor(private bikiniSvc: BikiniService) {}
 
+  async ngOnInit() {
+    this.bikiniArr = await this.bikiniSvc.getBrandBikini('Tezenis');
+    console.log(this.bikiniArr);
+  }
 }

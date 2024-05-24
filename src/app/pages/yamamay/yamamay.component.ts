6fbc1578814a4408f5131f3920e75a1772ecfd49
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-24 13:08:34 +0200

    chore: Update hero component layout and add Yamamay page

--- src/app/pages/yamamay/yamamay.component.ts
+++ src/app/pages/yamamay/yamamay.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BikiniService } from '../../bikini.service';
+import { iBikini } from '../../Models/i-bikini';
 
 @Component({
   selector: 'app-yamamay',
   templateUrl: './yamamay.component.html',
-  styleUrl: './yamamay.component.scss'
+  styleUrl: './yamamay.component.scss',
 })
 export class YamamayComponent {
+  bikiniArr: iBikini[] = [];
+  constructor(private bikiniSvc: BikiniService) {}
 
+  async ngOnInit() {
+    this.bikiniArr = await this.bikiniSvc.getBrandBikini('Yamamay');
+    console.log(this.bikiniArr);
+  }
 }

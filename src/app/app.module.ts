1efd82e665fdacb36ead0c234c8bceb53ed67686
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-24 10:27:36 +0200

    chore: Add main components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { BrandComponent } from './pages/brand/brand.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { CardComponent } from './main-components/card/card.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { HeaderComponent } from './main-components/header/header.component';
+import { HeroComponent } from './main-components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     BrandComponent,
-    Page404Component
+    Page404Component,
+    CardComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,

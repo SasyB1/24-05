d6f1b19060a3aa2b6ea5f38d00c105ce44269faf
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-24 14:16:59 +0200

    chore: Update header component with sticky navbar and scroll behavior

--- src/app/main-components/header/header.component.ts
+++ src/app/main-components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  @HostListener('window:scroll')
+  onWIndowScroll() {
+    let navbarColorChange = document.querySelector('.navbar');
+    if (navbarColorChange)
+      if (window.scrollY > 1) {
+        navbarColorChange.classList.add('scroll');
+      } else {
+        navbarColorChange.classList.remove('scroll');
+      }
+  }
 }
--- src/app/pages/calzedonia/calzedonia.component.ts
+++ src/app/pages/calzedonia/calzedonia.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BikiniService } from '../../bikini.service';
+import { iBikini } from '../../Models/i-bikini';
 
 @Component({
   selector: 'app-calzedonia',
   templateUrl: './calzedonia.component.html',
-  styleUrl: './calzedonia.component.scss'
+  styleUrl: './calzedonia.component.scss',
 })
 export class CalzedoniaComponent {
+  bikiniArr: iBikini[] = [];
+  constructor(private bikiniSvc: BikiniService) {}
 
+  async ngOnInit() {
+    this.bikiniArr = await this.bikiniSvc.getBrandBikini('Calzedonia');
+    console.log(this.bikiniArr);
+  }
 }

d7456e456383fedfcf26968676ec75ff8d4ff971
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-24 10:26:24 +0200

    feat: Add homepage, brand, and 404 routes
    This commit adds new routes for the homepage, brand, and 404 pages. The homepage route redirects to '/homepage' and displays the HomepageComponent. The brand route accepts a parameter for the brand and displays the BrandComponent. The 404 route displays the Page404Component for any other unknown routes.
    
    Co-authored-by: [Author Name]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { BrandComponent } from './pages/brand/brand.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/homepage',
+    pathMatch: 'full',
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent,
+    title: 'Homepage',
+  },
+  {
+    path: 'brand/:brand',
+    component: BrandComponent,
+    title: 'Homepage',
+  },
+  {
+    path: '**',
+    component: Page404Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { BrandComponent } from './pages/brand/brand.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BrandComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
